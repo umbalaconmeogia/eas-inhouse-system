046d28c0ef48c316a6f274984a021edd0362a147
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2016-11-10 18:45:58 +0900

    Complete login by login user

--- src/eas-inhouse-system/components/yii2-batsg/migrations/BaseMigration.php
+++ src/eas-inhouse-system/components/yii2-batsg/migrations/BaseMigration.php
@@ -8,5 +8,53 @@ use yii\db\Migration;
  */
 class BaseMigration extends Migration
 {
+    /**
+     * Create a table with specified columns, including the column bellow automatically.
+     * <ul>
+     * <li>id</li>
+     * <li>data_status</li>
+     * <li>create_time</li>
+     * <li>create_user_id</li>
+     * <li>update_time</li>
+     * <li>update_user_id</li>
+     * </ul>
+     *
+     * @param string $table
+     * @param string[] $columns
+     */
+    protected function createTableWithExtraFields($table, $columns)
+    {
+        // Prepare column definition of the table.
+        $columns = array_merge(
+            [
+                'id' => $this->text(),
+                'data_status' => $this->integer()->defaultValue(1),
+                'created_by' => $this->text(),
+                'created_at' => $this->integer(),
+                'updated_by' => $this->text(),
+                'updated_at' => $this->integer(),
+            ],
+            $columns
+        );
+        // Create table.
+        $this->createTable($table, $columns);
+        // Set primary key.
+        $this->addPrimaryKey("{$table}_pkey", $table, 'id');
+    }
 
+    /**
+     * Add comment on columns and table.
+     * @param string $table
+     * @param string $tableComment
+     * @param array $columnComments Mapping between column name and its comment.
+     */
+    protected function addComments($table, $tableComment = NULL, $columnComments = [])
+    {
+        foreach ($columnComments as $column => $comment) {
+            $this->addCommentOnColumn($table, $column, $comment);
+        }
+        if ($tableComment) {
+            $this->addCommentOnTable($table, $tableComment);
+        }
+    }
 }
--- src/eas-inhouse-system/migrations/m161004_173216_create_login_user_table.php
+++ src/eas-inhouse-system/migrations/m161004_173216_create_login_user_table.php
@@ -19,32 +19,21 @@ class m161004_173216_create_login_user_table extends BaseMigration
     public function safeUp()
     {
         // Create table.
-        $this->createTable($this->table, [
-            'id' => $this->text(),
+        $this->createTableWithExtraFields($this->table, [
             'username' => $this->text()->unique()->notNull(),
             'password_encryption' => $this->text()->notNull(),
             'access_token' => $this->text(),
             'auth_key' => $this->text(),
-            'data_status' => $this->integer()->defaultValue(1),
-            'created_by' => $this->text(),
-            'created_at' => $this->integer(),
-            'updated_by' => $this->text(),
-            'updated_at' => $this->integer(),
+            'must_change_password' => $this->integer(),
         ]);
-        // Set primary key.
-        $this->addPrimaryKey("{$this->table}_pkey", $this->table, 'id');
         // Add comment.
-        $this->addCommentOnTable($this->table, 'Login user');
-        $this->addCommentOnColumn($this->table, 'id', 'User id');
-        $this->addCommentOnColumn($this->table, 'username', 'Login username');
-        $this->addCommentOnColumn($this->table, 'password_encryption', 'Password encryption');
-        $this->addCommentOnColumn($this->table, 'access_token', 'Access token');
-        $this->addCommentOnColumn($this->table, 'auth_key', 'For cookie-based login');
-        $this->addCommentOnColumn($this->table, 'data_status', 'Data status');
-        $this->addCommentOnColumn($this->table, 'created_by', 'Create user id');
-        $this->addCommentOnColumn($this->table, 'created_at', 'Create timestamp');
-        $this->addCommentOnColumn($this->table, 'updated_by', 'Update user id');
-        $this->addCommentOnColumn($this->table, 'updated_at', 'Create timestamp');
+        $this->addComments($this->table, 'Login user', [
+            'username' => 'Login username',
+            'password_encryption' => 'Password encryption',
+            'access_token' => 'Access token',
+            'auth_key' => 'For cookie-based login',
+            'must_change_password' => '1: Must change password when login',
+        ]);
 
         $this->createUserAdmin();
     }
